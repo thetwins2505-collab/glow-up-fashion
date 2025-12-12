5f321daa78bd9dbc1a7dbd1d788e52f3a8a18383
thetwins2505-collab <thetwins2505@gmail.com>
2025-12-12 11:30:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontSize: "30px" }}>
+      Glow Up Fashion – Works ✔
+    </div>
+  );
+}
 
